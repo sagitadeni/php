7e3645a3935e553ce1d274de3825cdd99cf06168
denisagita80 <denisagita80@gmail.com>
2018-05-29 20:45:55 +0700

    Add proses and uploads file

---
+++ uploads.php
@@ -0,0 +1,5 @@
+<h1>Import Data Pegawai</h1>
+<form method="post" enctype="multipart/form-data" action="proses.php">
+ Silakan Pilih File Excel : <input name="userfile" type="file">
+ <input name="upload" type="submit" value="import">
+ </form>
